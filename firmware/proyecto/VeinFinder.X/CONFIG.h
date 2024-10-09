1f255223436eecc1648c3a4036bad34e2dbf7374
REGIOIGER <jos_raul@yahoo.com>
2024-10-09 12:06:59 -0600

    feat: integrated level selection and PWM output

---
+++ firmware/proyecto/VeinFinder.X/CONFIG.h
@@ -0,0 +1,9 @@
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef XC_HEADER_TEMPLATE_H
+#define	XC_HEADER_TEMPLATE_H
+
+
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
